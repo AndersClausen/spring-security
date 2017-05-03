15b0f50545dd090ddce27d23d24dbf1340c7776c
Anders Clausen <anders.s.clausen@gmail.com>
2017-05-03 13:44:03 +0100

    Updated file with @author

--- src/main/java/com/example/AwsApplication.java
+++ src/main/java/com/example/AwsApplication.java
@@ -22,6 +22,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+/**
+ *
+ * @author Anders Clausen
+ */
 @SpringBootApplication
 @RestController
 public class AwsApplication {
